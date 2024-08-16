4c9a1eae43460709405692f1e4f7962076cd4615
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-16 20:02:21 +0530

    Update screener_bs4.py

--- screener_bs4.py
+++ screener_bs4.py
@@ -58,6 +58,7 @@ if response.url == "https://www.screener.in/dash/":
         # print(df)
         df = df.transpose()
         df.columns = df.iloc[0]
+        df = df[1:]
         print(df)
         # df.to_csv('profit_and_loss.csv' , index=False)
 
