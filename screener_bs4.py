ab5b45ea325bbab0314b4ff8023969dee2667cb8
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-19 13:05:46 +0530

    Update screener_bs4.py

--- screener_bs4.py
+++ screener_bs4.py
@@ -57,9 +57,10 @@ if response.url == "https://www.screener.in/dash/":
         df = pd.DataFrame(row_data , columns=headers)
         # print(df)
         df = df.transpose()
-        df.columns = df.iloc[0]
+        # df.columns = df.iloc[0]
+        
+        df.columns = ["Sales","Expenses","Operating Profit","OPM" ,"Other Income", "Interest" , "Depreciation" , "Profit before tax" , "Tax" , "Net Profit" , "EPS in Rs" ,  "Dividend Payout" ]
         print(df.columns)
-        # df.columns = ["Date","Sales","Expenses","Operating Profit","OPM" ,"Other Income", "Interest" , "Depreciation" , "Profit before tax" , "Tax" , "Net Profit" , "EPS in Rs" ,  "Dividend Payout" ]
         df = df[1:]
         print(df)
         # df.to_csv('profit_and_loss.csv' , index=False)
