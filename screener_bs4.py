88abaf5869b4ea15dfba595b90576176de952f3e
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-19 17:51:44 +0530

    Update screener_bs4.py

--- screener_bs4.py
+++ screener_bs4.py
@@ -65,8 +65,8 @@ if response.url == "https://www.screener.in/dash/":
         print(df)
         # df.to_csv('profit_and_loss.csv' , index=False)
         for i in df.iloc[:,1:].columns:
-        df[i] = df[i].str.replace(',','').str.replace('%','').apply(eval)
-        print("Row data : ",df[i])
+           df[i] = df[i].str.replace(',','').str.replace('%','').apply(eval)
+           print("Row data : ",df[i])
 
         db_string = "postgresql+psycopg2://postgres:password@192.168.1.103:5432/sourcedb"
  
