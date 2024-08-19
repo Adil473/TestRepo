6f5e0c5fd96cc46f1c316ca5a9ef2bb75fc254fe
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-19 18:01:06 +0530

    Update screener_bs4.py

--- screener_bs4.py
+++ screener_bs4.py
@@ -56,10 +56,10 @@ if response.url == "https://www.screener.in/dash/":
 
         df = pd.DataFrame(row_data , columns=headers)
         # print(df)
-        df = df.transpose()
-        # df.columns = df.iloc[0]
+        # df = df.transpose()
+        df.columns = df.iloc[0]
         
-        df.columns = ["Sales","Expenses","Operating Profit","OPM" ,"Other Income", "Interest" , "Depreciation" , "Profit before tax" , "Tax" , "Net Profit" , "EPS in Rs" ,  "Dividend Payout" ]
+        # df.columns = ["Sales","Expenses","Operating Profit","OPM" ,"Other Income", "Interest" , "Depreciation" , "Profit before tax" , "Tax" , "Net Profit" , "EPS in Rs" ,  "Dividend Payout" ]
         print(df.columns)
         df = df[1:]
         print(df)
