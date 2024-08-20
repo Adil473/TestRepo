b275eb784a0f6601a54b701f80c70f684c91efa1
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-20 14:55:48 +0530

    Update screener_bs4.py

--- screener_bs4.py
+++ screener_bs4.py
@@ -57,11 +57,11 @@ if response.url == "https://www.screener.in/dash/":
         df = pd.DataFrame(row_data , columns=headers)
         # print(df)
         df = df.transpose()
-        # df.columns = df.iloc[0]
+        df.columns = df.iloc[0]
         
         # df.columns = ["Sales","Expenses","Operating Profit","OPM" ,"Other Income", "Interest" , "Depreciation" , "Profit before tax" , "Tax" , "Net Profit" , "EPS in Rs" ,  "Dividend Payout" ]
         print(df.columns)
-        # df = df[1:]
+        df = df[1:]
         print(df)
         # df.to_csv('profit_and_loss.csv' , index=False)
         # for i in df.iloc[:,1:].columns:
@@ -74,7 +74,7 @@ if response.url == "https://www.screener.in/dash/":
         engine = create_engine(db_string)
  
         # Assuming df is your DataFrame
-        df.to_sql('profit_loss_table_data', con=engine, index=False, if_exists='replace')
+        df.to_sql('profit_loss_data', con=engine, index=False, if_exists='replace')
         print("data written successfully")
 
         
