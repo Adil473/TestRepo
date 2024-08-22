8ba951d84ca90c7929aaa509f978cb880869fbf7
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-22 18:19:22 +0530

    Update retrieve.sh

--- retrieve.sh
+++ retrieve.sh
@@ -21,17 +21,25 @@
 export VAULT_ADDR='http://192.168.1.103:8200'
 export VAULT_TOKEN='myroot'
 
-email=$(vault kv get -field=email secret/my_secret)
-password=$(vault kv get -field=password secret/my_secret)
+EMAIL=$(vault kv get -field=email secret/my_secret)
+PASSWORD=$(vault kv get -field=password secret/my_secret)
 
-echo "export EMAIL=$email" > credentials/secrets.sh
-echo "export PASSWORD=$password" >> credentials/secrets.sh
+echo "$EMAIL" > credentials/email.txt
+echo "$PASSWORD" > credentials/password.txt
 
-chmod +x credentials/secrets.sh
-#contents of secrets-output
+# Print the credentials for debugging (ensure this is secure and not used in production)
+echo "Fetched Email: $EMAIL"
+echo "Fetched Password: $PASSWORD"
 
-ls -l credentials
 
-cat credentials/secrets.sh
-echo "Fetched email: $EMAIL"
-echo "Fetched password: $PASSWORD"
+# echo "export EMAIL=$email" > credentials/secrets.sh
+# echo "export PASSWORD=$password" >> credentials/secrets.sh
+
+# chmod +x credentials/secrets.sh
+# #contents of secrets-output
+
+# ls -l credentials
+
+# cat credentials/secrets.sh
+# echo "Fetched email: $EMAIL"
+# echo "Fetched password: $PASSWORD"
