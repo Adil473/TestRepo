36379112ebb7ff9c0c57e2ca1e4345a4c15be783
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-26 16:37:47 +0530

    Update retrieve.sh

--- retrieve.sh
+++ retrieve.sh
@@ -23,16 +23,24 @@ export VAULT_TOKEN='myroot'
 
 EMAIL=$(vault kv get -field=email secret/my_secret)
 PASSWORD=$(vault kv get -field=password secret/my_secret)
+PG_USER=$(vault kv get -field=PG_USER secret/my_secret)
+PG_PASS=$(vault kv get -field=PG_PASS secret/my_secret)
+PG_DB=$(vault kv get -field=PG_DB secret/my_secret)
 
 mkdir -p credentials
 chmod +x credentials
 echo "$EMAIL" > credentials/email.txt
 echo "$PASSWORD" > credentials/password.txt
+echo "$PG_USER" > credentials/pg_user.txt
+echo "$PG_PASS" > credentials/pg_pass.txt
+echo "$PG_DB" > credentials/pg_db.txt
 
 # Print the credentials for debugging (ensure this is secure and not used in production)
 echo "Fetched Email: $EMAIL"
 echo "Fetched Password: $PASSWORD"
-
+echo "Fetched PG_USER: $PG_USER"
+echo "Fetched PG_PASS: $PG_PASS"
+echo "Fetched PG_DB: $PG_DB"
 
 # echo "export EMAIL=$email" > credentials/secrets.sh
 # echo "export PASSWORD=$password" >> credentials/secrets.sh
@@ -40,7 +48,7 @@ echo "Fetched Password: $PASSWORD"
 # chmod +x credentials/secrets.sh
 # #contents of secrets-output
 
-# ls -l credentials
+ls -l credentials
 
 # cat credentials/secrets.sh
 # echo "Fetched email: $EMAIL"
