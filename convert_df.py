55b1c9658001cd0d630e4434d0e50636d42a8d69
Adil Shaikh <107281275+Adil473@users.noreply.github.com>
2024-08-26 17:56:42 +0530

    Update convert_df.py

--- convert_df.py
+++ convert_df.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import os
 import win32com.client
+import xlwings as xw
 
 def read_profit_and_loss_tab(file_name):
    if file_name:
        # Read the Excel file
        try:
-           xlapp = win32com.client.DispatchEx("Excel.Application")
-           wb = xlapp.Workbooks.Open("Reliance Industr.xlsx")
-           wb.RefreshAll()
-           wb.Save()
-            # Quit
-           xlapp.Quit()
-          # Load only the "Profit and Loss" sheet
+           # xlapp = win32com.client.DispatchEx("Excel.Application")
+           # wb = xlapp.Workbooks.Open("Reliance Industr.xlsx")
+           # wb.RefreshAll()
+           # wb.Save()
+           #  # Quit
+           # xlapp.Quit()
+
+           workbook = xw.Book("Reliance Industr.xlsx")
+           # Refresh all data connections in the workbook
+           for connection in workbook.api.Connections:
+               connection.refresh()
+
+           # Save and close the updated workbook
+           workbook.save("Reliance Industr.xlsx")
+           workbook.close()
+           # Load only the "Profit and Loss" sheet
            profit_and_loss_df = pd.read_excel(file_name, sheet_name="Profit & Loss")
            # Perform any additional processing here if needed
            print("Profit and Loss Data:")
